60bf8bbb23984a38cb85dccc7f75f3aff9a95a67
Monqui12 <monjairo12@hotmail.com>
2023-11-17 17:22:38 -0500

    subir cambios

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from 'src/app/services/database.service';
-
+import { LoadChildren, Router } from '@angular/router';
+import { AlertController, LoadingController, Platform, ToastController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +10,71 @@ import { DatabaseService } from 'src/app/services/database.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor(private database: DatabaseService) { }
+  credentialsForm!: FormGroup;
+  
+  constructor(
+    private database: DatabaseService,
+    public router:Router,
+    private platform: Platform,
+    private alertController: AlertController,
+    private formBuilder:FormBuilder,
+    private loadingController:LoadingController,
+    private toastController:ToastController
+  ) { }
 
   ngOnInit() {
-    this.database.initializPlugin().then(r => {
-      this.database.getUsers().then(result => {
-        console.log(result.values)
-      })
-    }).catch(error => {
-
-    })
+    this.platform.ready().then(()=>{
+      this.validaSesion().then((r) => {
+        if(r){
+          this.router.navigate(['/home']);
+          return;
+        }else{
+          // this.showAlert('Falso');
+        }
+      }).catch((er) => {
+        this.showAlert('Cierra y vuelve a abrir la app' + er,'Error!');
+      });
+    });  
+    this.credentialsForm = this.formBuilder.group({
+          identificacion: ['', [Validators.required]],
+          password: ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
-
+  async validaSesion(){
+    return await this.database.getUserSesion().then(res => {
+      if(res == true){
+        return true;
+      }else{
+        return false;
+      }
+    }).catch((er) => {
+      return false;
+    });
+  }
+  async login(){
+    const loading = this.loadingController.create({message: 'Iniciando Sesión...'});
+    loading.then(loading => loading.present());
+    this.presentToast('Sesion iniciada');
+    loading.then(loading => loading.dismiss());
+    this.router.navigate(['/home']);
+    this.credentialsForm.value
+  }
+  showAlert(msg:any, header:any) {
+    let alert = this.alertController.create({
+      message: msg,
+      header: header,
+      buttons: [{text: 'Aceptar'}]
+    });
+    alert.then(alert => alert.present());
+  }
+  async presentToast(msg:any) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+  delete(){
+    this.database.cerrarSesion();
+  }
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { SQLiteConnection, CapacitorSQLite, SQLiteDBConnection } from '@capacitor-community/sqlite';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { BehaviorSubject } from 'rxjs';
+import { Platform } from '@ionic/angular';
 
 const DB_HOTEL = 'myhoteldb';
 
@@ -7,46 +10,80 @@ const DB_HOTEL = 'myhoteldb';
   providedIn: 'root'
 })
 export class DatabaseService {
-  private sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite);
-  private db!: SQLiteDBConnection;
+  private databaseObj!: SQLiteObject;
+  private isDbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  tables = {
+    users: 'users',
+    sesion: 'user_sesion'
+  }
+  constructor(private sqlite: SQLite, private platform: Platform) {
+    this.createDatabase();
+  }
+  async createDatabase() {
+    this.platform.ready().then(() => {
+      this.sqlite.create({
+        name: 'hotel.db',
+        location: 'default'
+      })
+        .then((db: SQLiteObject) => {
+          this.databaseObj = db;
+          console.log("Database Creada");
+          this.createTables();
+        })
+        .catch(e => console.log(e));
+    })
+  }
+  dbState() {
+    return this.isDbReady.asObservable();
+  }
+  async createTables() {
+    this.databaseObj.executeSql(
+      `CREATE TABLE IF NOT EXISTS ${this.tables.users} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        nombres VARCHAR(100) NOT NULL,
+        apellidos VARCHAR(100) NOT NULL,
+        identificacion VARCHAR(30) NOT NULL,
+        correo VARCHAR(100) NOT NULL,
+        id_rol INTEGER NOT NULL
+      )`,
+      []
+    ).then((r) => {
+      console.log(`tabla de ${this.tables.users} creada`);
+    });
 
-  constructor(
-  ) {
-    //this.initializPlugin();
-   }
-  async initializPlugin(){
-    this.db = await this.sqlite.createConnection(
-      DB_HOTEL,
-      false,
-      'no-encryption',
-      1,
-      false
-    );
-    await this.db.open();
-    const  roles = `CREATE TABLE IF NOT EXISTS roles (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      rol varchar(40)
-      
-    )`;
-    await this.db.execute(roles);
-    const  users = `CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      identificacion varchar(30) NOT NULL,
-      email varchar(150) NOT NULL,
-      nombres varchar(100) NOT NULL,
-      apellidos varchar(100) NOT NULL,
-      id_rol integer NOT NULL
-    )`;
-    await this.db.execute(users);
-    this.creteUser();
-    return true
+    // Sesion user
+    this.databaseObj.executeSql(
+      `CREATE TABLE IF NOT EXISTS ${this.tables.sesion} (id INTEGER PRIMARY KEY autoincrement, user varchar(30),
+          fecha_sistema datetime)`,
+      []
+    ).then((r) => {
+      console.log(`tabla de ${this.tables.sesion} creada`);
+    });
   }
-  async creteUser(){
-    const users = await this.db.query(`INSERT INTO users(identificacion, email, nombres, apellidos, id_rol) values('1024', 'jjbariosm@unadvirtual.edu.co', 'John', 'Barrios', 1);`);
+  async getUserSesion(){
+     return await this.sqlite.create({
+      name: 'hotel.db',
+      location: 'default'
+      }).then((db: SQLiteObject) => {
+      this.databaseObj = db;
+      return this.databaseObj.executeSql(
+        `SELECT * FROM ${this.tables.sesion} ORDER BY fecha_sistema DESC`,
+        []
+      ).then((res) => {
+        if(res.rows.length > 0){
+          return true;
+        }else{
+          return false;
+        }
+      }).catch((er) => {
+        return false;
+      });
+    }).catch((e) => {
+      return false;
+    });
   }
-  async getUsers(){
-    const users = await this.db.query(`SELECT * FROM users;`);
-    console.log(users);
-    return users;
+  async cerrarSesion() {
+    this.databaseObj.executeSql(`DROP TABLE IF EXISTS ${this.tables.users}`);
+    this.databaseObj.executeSql(`DROP TABLE IF EXISTS ${this.tables.sesion}`);
   }
 }
